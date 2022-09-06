0650ccd9db3417a328f0625f76a30c81e9754f76
Christopher Agland-Williams <9oot85w9@alt.agland.uk>
2022-09-06 22:40:56 +0100

    Update script.sh

--- script.sh
+++ script.sh
@@ -18,15 +18,15 @@ pacman -S warpinator --noconfirm
 flatpak run space.crankshaft.Crankshaft
 curl -L https://github.com/SteamDeckHomebrew/decky-loader/raw/main/dist/install_prerelease.sh | sh
 
-wget "https://assist.zoho.eu/api/v2/install_urs?type=2&encapiKey=yA6KbntfvQylyz1XSEk715iD8Y01pa9q3nnjtS7lKcYheIayiKE%2BhhRoJ4GzI2be0ICF5%2F8Aao8Vc9rruI1WfJA9MtVUK5TGTuv4P2uV48xhu9mNdN8uhJyoA7EQEqJAcBks&app=linux&version=64bit&isDebian=false" -O zohoassist_1.0.0.1.zip
+wget "https://assist.zoho.eu/api/v2/install_urs?type=2&encapiKey=yA6KbntfvQylyz1XSEk715iD8Y01pa9q3nnjtS7lKcYheIayiKE%2BhhRoJ4GzI2be0ICF5%2F8Aao8Vc9rruI1WfJA9MtVUK5TGTuv4P2uV48xhu9mNdN8uhJyoA7EQEqJAcBks&app=linux&version=64bit&isDebian=false" -P /home/deck/stuff/ -O zohoassist_1.0.0.1.zip
 
-unzip zohoassist_1.0.0.1.zip && chmod +x install.bin && sudo ./install.bin
+unzip zohoassist_1.0.0.1.zip && chmod +x install.bin && sudo /home/deck/stuff/install.bin
 printf "$PASSWORD\n" | sudo systemctl enable ZohoAssistUrs.service
 printf "$PASSWORD\n" | sudo systemctl start ZohoAssistUrs.service
 
 
-wget https://raw.githubusercontent.com/krum110487/SSHDeck/main/bin/Install.sh
-printf "$PASSWORD\n" | sudo cd "/home/deck/Downloads"; chmod +x ./Install.sh; ./Install.sh
+wget https://raw.githubusercontent.com/krum110487/SSHDeck/main/bin/Install.sh -P /home/deck/stuff/
+printf "$PASSWORD\n" | sudo cd "/home/deck/Downloads"; chmod +x /home/deck/stuff/Install.sh; /home/deck/stuff/Install.sh
 
 # re-enable the readonly filesystem
 steamos-readonly enable
