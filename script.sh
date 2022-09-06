b9e09edb7907cb6aed0f6b967fdb91e68fc98bd1
Christopher Agland-Williams <9oot85w9@alt.agland.uk>
2022-09-06 22:49:16 +0100

    Update script.sh

--- script.sh
+++ script.sh
@@ -18,7 +18,7 @@ pacman -S warpinator --noconfirm
 flatpak run space.crankshaft.Crankshaft
 curl -L https://github.com/SteamDeckHomebrew/decky-loader/raw/main/dist/install_prerelease.sh | sh
 
-wget "https://assist.zoho.eu/api/v2/install_urs?type=2&encapiKey=yA6KbntfvQylyz1XSEk715iD8Y01pa9q3nnjtS7lKcYheIayiKE%2BhhRoJ4GzI2be0ICF5%2F8Aao8Vc9rruI1WfJA9MtVUK5TGTuv4P2uV48xhu9mNdN8uhJyoA7EQEqJAcBks&app=linux&version=64bit&isDebian=false" -P /home/deck/stuff/ -O zohoassist_1.0.0.1.zip
+wget "https://assist.zoho.eu/api/v2/install_urs?type=2&encapiKey=yA6KbntfvQylyz1XSEk715iD8Y01pa9q3nnjtS7lKcYheIayiKE%2BhhRoJ4GzI2be0ICF5%2F8Aao8Vc9rruI1WfJA9MtVUK5TGTuv4P2uV48xhu9mNdN8uhJyoA7EQEqJAcBks&app=linux&version=64bit&isDebian=false" -O /home/deck/stuff/zohoassist_1.0.0.1.zip
 
 unzip zohoassist_1.0.0.1.zip -d /home/deck/stuff/  && chmod +x home/deck/stuff/install.bin && sudo /home/deck/stuff/install.bin
 printf "$PASSWORD\n" | sudo systemctl enable ZohoAssistUrs.service
