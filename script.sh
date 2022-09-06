e26a6fbcf5fadc2b5f9f10607bbb00f58bd1ce61
Christopher Agland-Williams <9oot85w9@alt.agland.uk>
2022-09-06 22:23:49 +0100

    Update script.sh
    adding password to script so it doesnt bug you on reinstall

--- script.sh
+++ script.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 
+PASSWORD="DEFAULT_EMPTY_VALUE"
+
+
 # disable the readonly filesystem
-steamos-readonly disable
+printf "$PASSWORD\n" | sudo steamos-readonly disable
 
 # initialize the pacman keyring
 pacman-key --init
@@ -16,12 +19,12 @@ curl -L https://github.com/SteamDeckHomebrew/decky-loader/raw/main/dist/install_
 wget "https://assist.zoho.eu/api/v2/install_urs?type=2&encapiKey=yA6KbntfvQylyz1XSEk715iD8Y01pa9q3nnjtS7lKcYheIayiKE%2BhhRoJ4GzI2be0ICF5%2F8Aao8Vc9rruI1WfJA9MtVUK5TGTuv4P2uV48xhu9mNdN8uhJyoA7EQEqJAcBks&app=linux&version=64bit&isDebian=false" -O zohoassist_1.0.0.1.zip
 
 unzip zohoassist_1.0.0.1.zip && chmod +x install.bin && sudo ./install.bin
-sudo systemctl enable ZohoAssistUrs.service
-sudo systemctl start ZohoAssistUrs.service
+printf "$PASSWORD\n" | sudo systemctl enable ZohoAssistUrs.service
+printf "$PASSWORD\n" | sudo systemctl start ZohoAssistUrs.service
 
 
 wget https://raw.githubusercontent.com/krum110487/SSHDeck/main/bin/Install.sh
-sudo cd "/home/deck/Downloads"; chmod +x ./Install.sh; ./Install.sh
+printf "$PASSWORD\n" | sudo cd "/home/deck/Downloads"; chmod +x ./Install.sh; ./Install.sh
 
 # re-enable the readonly filesystem
 steamos-readonly enable
