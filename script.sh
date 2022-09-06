e4ab0b990f45fa4af35e10472051caad4ca21252
Christopher Agland-Williams <9oot85w9@alt.agland.uk>
2022-09-06 22:08:19 +0100

    Create script.sh
    just moving it from gist

---
+++ script.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+# disable the readonly filesystem
+steamos-readonly disable
+
+# initialize the pacman keyring
+pacman-key --init
+pacman-key --populate archlinux holo
+
+# install some packages used by the ark app to decompress some certain filetypes
+pacman -S p7zip unrar --noconfirm
+pacman -S warpinator --noconfirm
+flatpak run space.crankshaft.Crankshaft
+curl -L https://github.com/SteamDeckHomebrew/decky-loader/raw/main/dist/install_prerelease.sh | sh
+
+wget "https://assist.zoho.eu/api/v2/install_urs?type=2&encapiKey=yA6KbntfvQylyz1XSEk715iD8Y01pa9q3nnjtS7lKcYheIayiKE%2BhhRoJ4GzI2be0ICF5%2F8Aao8Vc9rruI1WfJA9MtVUK5TGTuv4P2uV48xhu9mNdN8uhJyoA7EQEqJAcBks&app=linux&version=64bit&isDebian=false" -O zohoassist_1.0.0.1.zip
+
+unzip zohoassist_1.0.0.1.zip && chmod +x install.bin && sudo ./install.bin
+sudo systemctl enable ZohoAssistUrs.service
+sudo systemctl start ZohoAssistUrs.service
+
+
+wget https://raw.githubusercontent.com/krum110487/SSHDeck/main/bin/Install.sh
+sudo cd "/home/deck/Downloads"; chmod +x ./Install.sh; ./Install.sh
+
+# re-enable the readonly filesystem
+steamos-readonly enable
